76a450bf38ef189db047bf89eddbcaedf8142d30
Peter Bacon Darwin <pete@bacondarwin.com>
2016-07-17 20:05:24 +0100

    feat(AppComponent): simplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,20 @@
 import {Component} from '@angular/core';
 import {StoreLogMonitorComponent} from '@ngrx/store-log-monitor';
-import {MyUppercasePipe} from '../shared/my-uppercase.pipe';
 import {CounterComponent} from './counter';
 
 @Component({
   selector: 'my-app',
-  pipes: [MyUppercasePipe],
   directives: [
     StoreLogMonitorComponent,
     CounterComponent
   ],
   template: `
-  <navbar>
-    <h1>{{ 'Hello Angular 2 and Webpack 2' | myUppercase}}</h1>
-  </navbar>
+    <h1>Angular 2, Webpack 2 and @ngrx/Store</h1>
 
-  <main>
+    <my-counter></my-counter>
 
-    <div>Your Content Here {{ name }}</div>
-    <button (click)="updateName('Other Person')">Change Name</button>
-
-  </main>
-
-  <my-counter></my-counter>
-
-  <ngrx-store-log-monitor toggleCommand="ctrl-t"></ngrx-store-log-monitor>
+    <ngrx-store-log-monitor toggleCommand="ctrl-t"></ngrx-store-log-monitor>
   `
 })
 export class AppComponent {
-  public name: string;
-  constructor() {
-    console.log('Hello Angular 2 Webpack 2');
-  }
-
-  updateName(value) {
-    this.name = value;
-  }
 }
